fb1508f2802dcda7edc67caede38f188ff1ec3d0
tnsgus375 <https://github.com/tnsgus375>
2026-05-29 16:05:27 +0900

    fix: import 정렬

--- app/repositories/schedule_repository.py
+++ app/repositories/schedule_repository.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 
-from app.models.medical_appointments import MedicalAppointment
 from app.models.favorite_places import FavoritePlace, PlaceType
+from app.models.medical_appointments import MedicalAppointment
 
 
 class ScheduleRepository:
